1daacd3b9e48a82f8ef634567759b55bf3d4beae
maryanasimpson20 <74646807+maryanasimpson20@users.noreply.github.com>
2020-12-28 16:56:57 +0100

    achieved character movement and set wall colliders
    - Wrote script for character movement that worked
    - Adjusted camera and light
    - Restricted character movement to within walls

--- BOO!/Assets/Ghost.cs
+++ BOO!/Assets/Ghost.cs
@@ -1,25 +1,35 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Ghost : MonoBehaviour
 {
-    private Rigidbody rb;
-    privat float moveSpeed;
-    private float dirX, dirZ;
-
+    private Vector2 direction;
+    Rigidbody rb;
+    public float speed;
+    public float dirY, dirZ;
 //Use below for initialization
-    private void Start () {
-      moveSpeed = 3.8f;
+    void Start() {
       rb = GetComponent<Rigidbody>();
     }
     //Update is called once per frame
-    private void Update () {
-      dirX = Input.GetAxis("Horizontal") * moveSpeed;
-      dirZ = Input.GetAxis("Vertical") * moveSpeed;
-    }
-    private void private void FixedUpdate() {
-      {
-        rb.velocity = new Vector3(dirX, rb.velocity.y, dirZ);
+    void Update() {
+      speed = 7f;
+      if(Input.GetAxis("Horizontal")<0){
+        direction = Vector2.left; //changing the direction of pacman to left
       }
+        
+      if(Input.GetAxis("Horizontal")>0){
+        direction = Vector2.right; //changing the direction of pacman to right
+       }
+
+      rb.velocity = direction*speed;
+       
+      /*dirX = Input.GetAxis("Horizontal") * moveSpeed;
+      dirZ = Input.GetAxis("Vertical") * moveSpeed;*/
+    }
+    /*private void FixedUpdate() {
+      {
+        rb.velocity = new Vector3(rb.velocity.x, dirY, dirZ);
+      }*/
 } 
