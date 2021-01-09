90bc3fa41329a8ad4c6bf30fc2015dcf0445149b
Mikscicluna <74646629+Mikscicluna@users.noreply.github.com>
2021-01-09 22:51:49 +0100

    reuploaded

--- BOO!/Assets/Ghost.cs
+++ BOO!/Assets/Ghost.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Ghost : MonoBehaviour
 {
@@ -10,9 +9,6 @@ public class Ghost : MonoBehaviour
     public float speed;
     public float dirY, dirZ;
 //Use below for initialization
-    void OnDestroy() {
-        SceneManager.LoadScene("Game Over");
-    }
     void Start() {
       rb = GetComponent<Rigidbody>();
     }
@@ -20,35 +16,15 @@ public class Ghost : MonoBehaviour
     void Update() {
       speed = 7f;
       if(Input.GetAxis("Horizontal")<0){
-        direction = Vector2.left; //changing the direction of ghost to left
+        direction = Vector2.left; //changing the direction of pacman to left
       }
         
       if(Input.GetAxis("Horizontal")>0){
-<<<<<<< Updated upstream:BOO!/Assets/Ghost.cs
-        direction = Vector2.right; //changing the direction of ghost to right
-      }
-
-    void OnTriggerEnter2D(Collider2D other)
-    {
-      if(other.tag == "Flashlight")
-      {
-        Destroy(this.gameObject);
-      }
-    }
-
-      rb.velocity = direction*speed;
-
-=======
         direction = Vector2.right; //changing the direction of pacman to right
-        }
+       }
 
       rb.velocity = direction*speed;
-      
-<<<<<<< Updated upstream:BOO!/Assets/Ghost.cs
->>>>>>> Stashed changes:BOO!/Assets/Scripts/Ghost.cs
-=======
->>>>>>> Stashed changes:BOO!/Assets/Scripts/Ghost.cs
-      
+       
       /*dirX = Input.GetAxis("Horizontal") * moveSpeed;
       dirZ = Input.GetAxis("Vertical") * moveSpeed;*/
     }
