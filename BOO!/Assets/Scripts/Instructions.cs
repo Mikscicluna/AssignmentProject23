5031571b8b1d50b6adbdb2468feabd42fcc66132
maryanasimpson20 <74646807+maryanasimpson20@users.noreply.github.com>
2021-01-13 15:10:22 +0100

    Finalized script for UI, created build
    - Finishing touches to UI scripts; added new Instructions page to script and made it so that when it is clicked in the options menu, it opens the instructions scene
    - Added script to last 2 back buttons
    -Fixed compiler error
    - Created build

--- BOO!/Assets/Scripts/Instructions.cs
+++ BOO!/Assets/Scripts/Instructions.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class Options : MonoBehaviour
+public class Instructions : MonoBehaviour
 {
     public bool isBack;
 
