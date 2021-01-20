a1d1966da01e3f17ff78f28456181b8a5a7f88f6
maryanasimpson20 <74646807+maryanasimpson20@users.noreply.github.com>
2021-01-20 13:03:18 +0100

    Fullscreen option & work on pause function
    - Made it possible for players to change from fullscreen to windowed from visuals settings
    - Worked on pause function; had to improvise since we built the game outside the UI canvas and do not want to risk shifting everything, however the code does not work yet. More research required

--- BOO!/Assets/Scripts/Visuals.cs
+++ BOO!/Assets/Scripts/Visuals.cs
@@ -7,6 +7,7 @@ public class Visuals : MonoBehaviour
 {
     // Start is called before the first frame update
     public bool isBack;
+    public bool isFullscreen;
 
     void OnMouseUp()
     {
@@ -15,6 +16,14 @@ public class Visuals : MonoBehaviour
             SceneManager.LoadScene("Options");
         }
     }
+    
+    //Fullscreen toggle to change between fullscreen and windowed
+    public void SetFullscreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+        Debug.Log("Changing Screen Settings"); //to check if it is working
+    }
+
     void Start()
     {
         
